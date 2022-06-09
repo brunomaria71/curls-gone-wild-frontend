0be75b3c3514862bc750f0f31a641dc93dafc027
brunomaria71 <brunomaria71@yahoo.com>
2022-06-09 11:23:23 -0400

    styling and fixing stuff so the cards are in rows

--- src/components/HairCards.js
+++ src/components/HairCards.js
@@ -1,49 +1,61 @@
-// import { Card, Col } from "antd";
-// import React from "react";
-// import { useNavigate } from "react-router-dom";
-
-// const { Meta } = Card;
-
-// export default function HairCard({ routines }) {
-//   let navigate = useNavigate();
-//   return (
-//     <Col style={{ width: "300px", margin: "1em" }}>
-//       <Card>
-//         <Meta title={routines?.type} description={routines?.address} />
-//       </Card>
-//     </Col>
-//   );
-// }
-
 import { Card, Col } from "antd";
 import React from "react";
-import image from "../assets/blackwavyhair.jpeg";
+import coily from "../assets/coily.jpeg";
+import curly from "../assets/curly.jpeg";
+import wavy from "../assets/wavy.jpeg";
 import "../styles/Cards.css";
+
 const { Meta } = Card;
 
-const HairCard = () => (
-  <Col style={{ width: "300px", margin: "1em" }}>
-    <Card
-      hoverable
-      style={{
-        width: 100,
-        height: 100,
-      }}
-      cover={<img alt="example" src={image} width="200px" height="200px" />}
-    >
-      <Meta title="curly" description="hair is curly" className="card" />
-    </Card>
-    {/* <Card
-      hoverable
-      style={{
-        width: 100,
-        height: 100,
-      }}
-      cover={<img alt="example" src={image2} width="200px" height="200px" />}
-    >
-      <Meta title="wavy" description="hair is wavy" className="card" />
-    </Card> */}
-  </Col>
+const HairCards = () => (
+  <div>
+    <Col className="card-group">
+      <div className="wavy-column">
+        <Card
+          hoverable
+          cover={
+            <img className="card-images" alt="wavy hair example" src={wavy} />
+          }
+        >
+          <Meta
+            title="wavy"
+            description="hair is wavy"
+            className="card-meta-title"
+          />
+        </Card>
+      </div>
+
+      <div className="curly-column">
+        <Card
+          hoverable
+          cover={
+            <img className="card-images" alt="curly hair example" src={curly} />
+          }
+        >
+          <Meta
+            title="curly"
+            description="hair is curly"
+            className="card-meta-title"
+          />
+        </Card>
+      </div>
+
+      <div className="coily-column">
+        <Card
+          hoverable
+          cover={
+            <img className="card-images" alt="curly hair example" src={coily} />
+          }
+        >
+          <Meta
+            title="coily"
+            description="hair is coily"
+            className="card-meta-title"
+          />
+        </Card>
+      </div>
+    </Col>
+  </div>
 );
 
-export default HairCard;
+export default HairCards;
