52f746623ba7c9a5ddfe730dc9b7699b36d9417b
brunomaria71 <brunomaria71@yahoo.com>
2022-06-13 08:32:30 -0400

    it works

--- src/components/HairCards.js
+++ src/components/HairCards.js
@@ -1,7 +1,6 @@
 import { Card, Col } from "antd";
 import React, { useContext } from "react";
 import { useNavigate } from "react-router";
-import { Link } from "react-router-dom";
 import coily from "../assets/coily.jpeg";
 import curly from "../assets/curly.jpeg";
 import wavy from "../assets/wavy.jpeg";
@@ -11,97 +10,91 @@ import "../styles/Cards.css";
 const { Meta } = Card;
 
 export default function HairCards() {
-  const { bestFor, setBestFor, type, setType } = useContext(UserChoiceContext);
+  const { setBestFor, setType } = useContext(UserChoiceContext);
   let navigate = useNavigate();
 
-  const setWavy = () => {
+  const onClickWavy = () => {
     setBestFor("wavy");
     setType("wavy");
-    navigate("/resultpage");
+    navigate("/results");
   };
 
   const onClickCurly = () => {
     setBestFor("curly");
     setType("curly");
-    navigate("/resultpage");
+    navigate("/results");
   };
 
   const onClickCoily = () => {
     setBestFor("coily");
     setType("coily");
-    navigate("/resultpage");
+    navigate("/results");
   };
 
   return (
     <div className="cards-overall">
       <Col className="card-group">
         <div className="wavy-column">
-          <Link to={"/resultpage"}>
-            <Card
-              id="card1"
-              hoverable
-              cover={
-                <img
-                  className="card-images"
-                  alt="wavy hair example"
-                  src={wavy}
-                  onClick={() => setWavy}
-                />
-              }
-            >
-              <Meta
-                title="wavy"
-                description="hair is wavy"
-                className="card-meta-title"
+          <Card
+            id="card1"
+            hoverable
+            cover={
+              <img
+                className="card-images"
+                alt="wavy hair example"
+                src={wavy}
+                onClick={onClickWavy}
               />
-            </Card>
-          </Link>
+            }
+          >
+            <Meta
+              title="wavy"
+              description="hair is wavy"
+              className="card-meta-title"
+            />
+          </Card>
         </div>
 
         <div className="curly-column">
-          <Link to={"/resultpage"}>
-            <Card
-              id="card1"
-              hoverable
-              cover={
-                <img
-                  className="card-images"
-                  alt="curly hair example"
-                  src={curly}
-                  onClick={() => onClickCurly}
-                />
-              }
-            >
-              <Meta
-                title="curly"
-                description="hair is curly"
-                className="card-meta-title"
+          <Card
+            id="card1"
+            hoverable
+            cover={
+              <img
+                className="card-images"
+                alt="curly hair example"
+                src={curly}
+                onClick={onClickCurly}
               />
-            </Card>
-          </Link>
+            }
+          >
+            <Meta
+              title="curly"
+              description="hair is curly"
+              className="card-meta-title"
+            />
+          </Card>
         </div>
 
         <div className="coily-column">
-          <Link to={"/resultpage"}>
-            <Card
-              id="card1"
-              hoverable
-              cover={
-                <img
-                  className="card-images"
-                  alt="curly hair example"
-                  src={coily}
-                  onClick={() => onClickCoily}
-                />
-              }
-            >
-              <Meta
-                title="coily"
-                description="hair is coily"
-                className="card-meta-title"
+          <Card
+            id="card1"
+            hoverable
+            cover={
+              <img
+                className="card-images"
+                alt="curly hair example"
+                src={coily}
+                onClick={() => onClickCoily}
               />
-            </Card>
-          </Link>
+            }
+          >
+            <Meta
+              title="coily"
+              description="hair is coily"
+              className="card-meta-title"
+            />
+          </Card>
         </div>
       </Col>
     </div>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,15 +1,22 @@
 import { useContext, useEffect } from "react";
 import { ResultsContext } from "../context/ResultsContext";
+import { UserChoiceContext } from "../context/UserChoiceContext";
 
 export default function Products() {
   const { products, setProducts } = useContext(ResultsContext);
+  const { bestFor, setBestFor } = useContext(UserChoiceContext);
 
   useEffect(() => {
     fetch("https://curls-gone-wild.web.app/products")
       .then((res) => res.json())
-      .then((data) => setProducts(data))
+      .then((data) => {
+        console.log("this is", bestFor);
+        const products = data.filter((prod) => prod.bestFor === bestFor);
+        setProducts(products);
+      })
       .catch((err) => console.error(err));
   }, []);
+
   return (
     <div>
       {!products ? (
@@ -26,3 +33,26 @@ export default function Products() {
     </div>
   );
 }
+
+// {
+//   _id:
+// 629e93411ef62a54272f7cf4
+// name
+// :
+// "Blueberry Bliss Reparative Hair Wash"
+// description
+// :
+// "Sulfate-free cleanser guaranteed to soften the driest kink, hydrate th..."
+// instructions
+// :
+// "Thoroughly soak hair in shower, detangle hair before shampooing for be..."
+// bestFor
+// :
+// "wavy"
+// type
+// :
+// "Shampoo"
+// image
+// :
+// "https://i5.walmartimages.com/asr/37e44f3a-db62-405b-af87-42c79dec13dd_..."
+// }
