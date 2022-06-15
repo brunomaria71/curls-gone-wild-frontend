d8aed5463e25ca9a2fce34a0af11aa76c8c937df
brunomaria71 <brunomaria71@yahoo.com>
2022-06-15 19:08:17 -0400

    menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -31,29 +31,3 @@ export default function Template() {
     </Layout>
   );
 }
-
-{
-  /* <Layout className="sidebar">
-  <Sider
-    breakpoint="lg"
-    collapsedWidth="0"
-    onBreakpoint={(broken) => {
-      console.log(broken);
-    }}
-    onCollapse={(collapsed, type) => {
-      console.log(collapsed, type);
-    }}
-  >
-    <a href="/home">
-      <img className="logo" alt="logo of curls gone wild" src={logo} />
-    </a>
-    <Menu>
-      <div className="nav-links">
-        <a href="/home">Home</a>
-        <a href="/reviews/add">Add Review</a>
-        <a href="/reviews">See What Others Are Saying</a>
-      </div>
-    </Menu>
-  </Sider>
-</Layout>; */
-}
